9aa4cd8a6d0b7bd321ae6ec4da578a5094490bd5
Subhad2000 <subhadeep.gupta@anchanto.com>
2021-02-10 22:57:23 +0530

    Armstrong number code done,All test case passed

--- source/armstrong_number.rb
+++ source/armstrong_number.rb
@@ -4,6 +4,18 @@ class ArmstrongNumber
     end
     
     def is_armstrong
-        true
+        
+        if @number<=0
+            return false
+        else
+            #Calculate length of the number
+            length=@number.to_s.length
+
+            #Check if sum of digits^length==number or not
+            sum=@number.to_s.split("").inject(0){|a,b| a+b.to_i**length}
+
+
+            sum==@number
+        end
     end
 end
